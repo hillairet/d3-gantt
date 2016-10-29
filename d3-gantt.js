4e8cbf55dce6e5e9aa3680a71930685ee53cb6bd
Ant <anthony.hillairet@gmail.com>
2016-10-29 09:23:16 -0700

    Initial commit. Basic features and structure done.

---
+++ d3-gantt.js
@@ -0,0 +1,548 @@
+(function () {
+	d3.gantt = function() {
+		var DISPLAY_TYPES = ["circle", "rect"];
+
+		var hover = function () {},
+		mouseover = function () {},
+		mouseout = function () {},
+		click = function () {},
+		scroll = function () {},
+		labelFunction = function(label) { return label; },
+		navigateLeft = function () {},
+		navigateRight = function () {},
+		orient = "bottom",
+		width = null,
+		height = null,
+		rowSeparatorsColor = null,
+		backgroundColor = null,
+		tickFormat = { format: d3.time.format("%I %p"),
+			tickTime: d3.time.hours,
+			tickInterval: 1,
+			tickSize: 6,
+			tickValues: null
+		},
+		colorCycle = d3.scale.category20(),
+		colorPropertyName = null,
+		display = "rect",
+		beginning = 0,
+		labelMargin = 0,
+		ending = 0,
+		margin = {left: 10, right:10, top: 30, bottom:10},
+		stacked = false,
+		rotateTicks = false,
+		itemHeight = 60,
+		itemMargin = 5,
+		navMargin = 60,
+		showTimeAxis = true,
+		showAxisTop = false,
+		showTodayLine = false,
+		timeAxisTick = false,
+		timeAxisTickFormat = {stroke: "stroke-dasharray", spacing: "4 10"},
+		showTodayFormat = {marginTop: 25, marginBottom: 0, width: 1, color: colorCycle},
+		showBorderFormat = {marginTop: 25, marginBottom: 0, width: 1, color: colorCycle},
+		showAxisCalendarYear = false,
+		axisBgColor = "white",
+		chartData = {},
+		axisDelay = 500
+		;
+
+//		beginning = 1355702800000;
+//		ending = 1355861910000;
+		rotateTicks = 45;
+		tickFormat = {
+			// format: d3.time.format("%d/%m %a"),
+			// tickTime: d3.time.days,
+			format: d3.time.format("%H:%M"),
+			tickTime: d3.time.hours,
+			tickInterval: 1,
+			tickSize: 2
+		};
+		// colorPropertyName = 'category',
+
+
+		function find_xrange(data){
+			// figure out beginning and ending times if they are unspecified
+			if ( ending === 0 || beginning === 0) {
+				var minTime = 0,
+				maxTime = 0;
+				data.forEach(function (entry, i) {
+					time = entry.times[0];
+					if (i === 0){
+						minTime = time.starting_time;
+						maxTime = time.starting_time;
+					}
+					if(beginning === 0)
+						if (time.starting_time < minTime )
+							minTime = time.starting_time;
+					if(ending === 0)
+						if (time.ending_time > maxTime)
+							maxTime = time.ending_time;
+				});
+				if ( beginning === 0)
+					beginning = minTime;
+				if ( ending === 0)
+					ending = maxTime;
+			}
+		}
+
+		///////////////////////////////////////////////////////////////////
+		function make_xaxis(x,xwdt){
+			var xScale = d3.time.scale()
+				.domain([beginning, ending])
+				.range([x, xwdt]);
+
+			var xAxis = d3.svg.axis()
+				.scale(xScale)
+				.orient(orient)
+				.tickFormat(tickFormat.format)
+				.tickSize(tickFormat.tickSize);
+
+			if (tickFormat.tickValues != null) {
+				xAxis.tickValues(tickFormat.tickValues);
+			} else {
+				xAxis.ticks(tickFormat.numTicks || tickFormat.tickTime, tickFormat.tickInterval);
+			}
+			return xAxis;
+		}
+
+
+		///////////////////////////////////////////////////////////////////
+		function make_yaxis(lbls, yhgt){
+			var yScale = d3.scale.ordinal()
+				.domain(lbls)
+				.rangeBands([margin.top, yhgt]);
+			return yScale;
+		}
+
+
+		///////////////////////////////////////////////////////////////////
+		function draw(data, init = false){
+			var svg = d3.select("svg");
+			svgbb = svg.node().getBBox();
+
+			// X axis
+			xAxis = make_xaxis(0,width+10);
+			
+			// Y axis
+			var labels = [];
+			data.forEach(function(entry,i){
+				labels.push(entry.label);
+			});
+
+			yheight = Math.max((labels.length * itemHeight), (height+10));
+			yScale = make_yaxis(labels,yheight);
+			var yAxis = d3.svg.axis()
+				.scale(yScale)
+				.orient("left")
+				.tickSize(2);
+
+			if (init){
+				var gxAxis = svg.append("g")
+					.attr("class", "xaxis")
+					.attr("transform", "translate(" + (-10) + "," + (height+10) + ")")
+					.call(xAxis);
+				var gyAxis = svg.append("g")
+					.attr("clip-path","url(#clip-chart)")
+					.append('g')
+					.attr("class", "yaxis")
+					.attr("transform", "translate(" + (-10) + "," + 0 + ")")
+					.call(yAxis);
+
+				var chart = svg.append("g")
+					.attr("class","chart")
+					.attr("clip-path","url(#clip-chart)");
+				var cp = svg.append("clipPath")
+					.attr("id","clip-chart");
+				cp.append("rect")
+					.attr("width",width)
+					.attr("height",height);
+			} else {
+				var gxAxis = d3.select("g.xaxis");
+				var gyAxis = d3.select("g.yaxis");
+				var chart = d3.select("g.chart");
+				var cp = d3.select("clipPath");
+			}
+
+
+			if (rotateTicks) {
+				gxAxis.selectAll(".tick text")
+					.attr("transform", function(d) {
+						return "rotate(" + rotateTicks + ")translate("
+							+ ((this.getBBox().width * Math.sin(rotateTicks)) / 2 ) + ","
+							+ ((this.getBBox().height * Math.cos(rotateTicks)) / 2) + ")";
+					});
+			}
+
+			if(!init){
+				// Update the axes to their correct position and
+				var gyabb = gyAxis.node().getBBox();
+				yScale = make_yaxis(labels,gyabb.height);
+				yAxis = d3.svg.axis()
+				.scale(yScale)
+				.orient("left")
+				.tickSize(2);
+				gyAxis.call(yAxis);
+				gyabb = gyAxis.node().getBBox();
+
+				xpos = gyabb.width + margin.left;
+				xwidth = width - xpos - margin.right;
+				xAxis = make_xaxis(xpos,xwidth);
+				gxAxis.transition()
+					.duration(axisDelay)
+					.call(xAxis);
+				var gxabb = gxAxis.node().getBBox();
+
+				yheight = Math.max(
+						(labels.length * itemHeight),
+						(height-gxabb.height-margin.bottom));
+				yScale = make_yaxis(labels,yheight);
+				yAxis = d3.svg.axis()
+				.scale(yScale)
+				.orient("left")
+				.tickSize(2);
+				gyAxis.transition()
+					.duration(axisDelay)
+					.call(yAxis);
+				
+				ytransX = (height-gxabb.height-margin.bottom);
+				xtransY = (gyabb.width+margin.left);
+				
+				gxAxis.transition().duration(axisDelay)
+					.attr("transform", "translate(" + 0 + "," + ytransX + ")");
+
+				gyAxis.transition().duration(axisDelay)
+					.attr("transform", "translate(" + xtransY + ',' + 0 + ")");
+
+				cpwidth = width - margin.right - xtransY;
+				cpheight = ytransX - margin.top;
+				cp.select("rect")
+					.attr("height",cpheight)
+					.attr("width", width)
+					.attr("x",0)
+					.attr("y",margin.top);
+
+				var rectdata = [];
+				data.forEach(function (entry, i) {
+					entry.times.forEach(function (time, j) {
+						var rect = {};
+						rect.x = xAxis.scale()(time.starting_time);
+						rect.width = xAxis.scale()(time.ending_time) - xAxis.scale()(time.starting_time);
+						rect.id = entry.label;
+						rectdata.push(rect);
+					});
+				});
+
+				// Draw the bar chart
+				var bars = chart.selectAll("rect.bar")
+					.data(rectdata)
+
+				//update
+				bars.style("fill", function(d, i){
+					  var dColorPropName;
+					  if (d.color) return d.color;
+					  if( colorPropertyName ){
+						dColorPropName = d[colorPropertyName];
+						if ( dColorPropName ) {
+						  return colorCycle( dColorPropName );
+						} else {
+						  return colorCycle( datum[colorPropertyName] );
+						}
+					  }
+					  return colorCycle(i);
+					});
+
+				//enter
+				bars.enter()
+					.append("rect")
+					.attr("class", "bar")
+					.attr("width", 0)
+					.attr("transform", function(d,i) {
+						return "translate(" + d.x +','+ yAxis.scale()(d.id) + ")"
+					})
+					.style("fill", function(d, i){
+					  var dColorPropName;
+					  if (d.color) return d.color;
+					  if( colorPropertyName ){
+						dColorPropName = d[colorPropertyName];
+						if ( dColorPropName ) {
+						  return colorCycle( dColorPropName );
+						} else {
+						  return colorCycle( datum[colorPropertyName] );
+						}
+					  }
+					  return colorCycle(i);
+					});
+
+				//exit 
+				bars.exit()
+				.transition()
+				.duration(axisDelay)
+				.ease("exp")
+					.attr("width", 0)
+					.remove();
+
+
+				bars.attr("stroke-width", 4)
+					.transition()
+					.delay(500)
+					.duration(axisDelay)
+					.ease("quad")
+					.attr("width", function(d,i){ return d.width; })
+					.attr("height", yAxis.scale().rangeBand())
+					.attr("transform", function(d,i) {
+						return "translate(" + d.x +','+ yAxis.scale()(d.id) + ")"
+					});
+
+				var pan = function() {
+					var deltaY = 0.0;
+					var yStart = d3.transform(gyAxis.attr("transform")).translate[1];
+					var fullHeight = gyAxis.node().getBBox().height;
+					var visibleHeight = cp.node().getBBox().height;
+					// scroll(yScale(y), yScale);
+					if (d3.event.sourceEvent.type === "wheel") {
+						// use the `d3.event.sourceEvent.deltaY` value to translate
+						yWheel = -1*d3.event.sourceEvent.deltaY;
+						var yNew = Math.min(0, Math.max(yStart+ yWheel, -1*(fullHeight - visibleHeight)));
+						deltaY = yNew - yStart;
+					} else if (d3.event.sourceEvent.type === "mousemove") {
+						// use the normal d3.event.translate array to translate
+						// e.g., yTranslation = d3.event.translate[1];
+					}
+					gyAxis.attr("transform", "translate(" + xtransY+"," + (yStart+deltaY) +")");
+					// Translate chart elements
+					chart.selectAll('*').each(function(){
+						var barPos = d3.transform(d3.select(this).attr("transform")).translate;
+						d3.select(this).attr("transform", "translate(" + barPos[0] +"," + (barPos[1]+deltaY) +")");
+					});
+				};
+
+				var panYOnScrollAndDrag = d3.behavior.zoom()
+					.scaleExtent([1, 1]) // prevents wheel events (or anything) from changing the scale
+					.on("zoom", pan);
+
+				svg.call(panYOnScrollAndDrag);
+			}
+
+		}
+
+		function gantt(gParent)
+		{
+			//setup the svg
+			var svg = gParent.append("svg")
+				.attr("width", "100%")
+				.attr("height", "100%");
+			svg.append("svg:rect")
+				.attr("width", "100%")
+				.attr("height", "100%")
+				.attr("stroke", "#000")
+				.attr("fill", "none");
+
+			var svg = d3.select("svg");
+			svgbb = svg.node().getBBox();
+			width = svgbb.width;
+			height = svgbb.height;
+
+			// Create xaxis group
+			// Create yaxis group
+
+			var data = svg.data()[0];
+
+			find_xrange(data);
+
+			// Create X and Y axes
+			draw(data,true);
+
+			// Draw everything
+			draw(data);
+
+			data2 = [];
+			data2.push(data[0]);
+			data2.push(data[2]);
+
+			d3.select("#data2")
+				.on("click", function(d,i) {
+					draw(data2)
+				})   
+
+
+		}
+		// SETTINGS
+
+		gantt.margin = function (p) {
+			if (!arguments.length) return margin;
+			margin = p;
+			return gantt;
+		};
+
+		gantt.orient = function (orientation) {
+			if (!arguments.length) return orient;
+			orient = orientation;
+			return gantt;
+		};
+
+		gantt.itemHeight = function (h) {
+			if (!arguments.length) return itemHeight;
+			itemHeight = h;
+			return gantt;
+		};
+
+		gantt.itemMargin = function (h) {
+			if (!arguments.length) return itemMargin;
+			itemMargin = h;
+			return gantt;
+		};
+
+		gantt.navMargin = function (h) {
+			if (!arguments.length) return navMargin;
+			navMargin = h;
+			return gantt;
+		};
+
+		gantt.height = function (h) {
+			if (!arguments.length) return height;
+			height = h;
+			return gantt;
+		};
+
+		gantt.width = function (w) {
+			if (!arguments.length) return width;
+			width = w;
+			return gantt;
+		};
+
+		gantt.display = function (displayType) {
+			if (!arguments.length || (DISPLAY_TYPES.indexOf(displayType) == -1)) return display;
+			display = displayType;
+			return gantt;
+		};
+
+		gantt.labelFormat = function(f) {
+			if (!arguments.length) return labelFunction;
+			labelFunction = f;
+			return gantt;
+		};
+
+		gantt.tickFormat = function (format) {
+			if (!arguments.length) return tickFormat;
+			tickFormat = format;
+			return gantt;
+		};
+
+		gantt.hover = function (hoverFunc) {
+			if (!arguments.length) return hover;
+			hover = hoverFunc;
+			return gantt;
+		};
+
+		gantt.mouseover = function (mouseoverFunc) {
+			if (!arguments.length) return mouseover;
+			mouseover = mouseoverFunc;
+			return gantt;
+		};
+
+		gantt.mouseout = function (mouseoutFunc) {
+			if (!arguments.length) return mouseout;
+			mouseout = mouseoutFunc;
+			return gantt;
+		};
+
+		gantt.click = function (clickFunc) {
+			if (!arguments.length) return click;
+			click = clickFunc;
+			return gantt;
+		};
+
+		gantt.scroll = function (scrollFunc) {
+			if (!arguments.length) return scroll;
+			scroll = scrollFunc;
+			return gantt;
+		};
+
+		gantt.colors = function (colorFormat) {
+			if (!arguments.length) return colorCycle;
+			colorCycle = colorFormat;
+			return gantt;
+		};
+
+		gantt.beginning = function (b) {
+			if (!arguments.length) return beginning;
+			beginning = b;
+			return gantt;
+		};
+
+		gantt.ending = function (e) {
+			if (!arguments.length) return ending;
+			ending = e;
+			return gantt;
+		};
+
+		gantt.labelMargin = function (m) {
+			if (!arguments.length) return labelMargin;
+			labelMargin = m;
+			return gantt;
+		};
+
+		gantt.rotateTicks = function (degrees) {
+			if (!arguments.length) return rotateTicks;
+			rotateTicks = degrees;
+			return gantt;
+		};
+
+		gantt.stack = function () {
+			stacked = !stacked;
+			return gantt;
+		};
+
+		gantt.showBorderFormat = function(borderFormat) {
+			if (!arguments.length) return showBorderFormat;
+			showBorderFormat = borderFormat;
+			return gantt;
+		};
+
+		gantt.showToday = function () {
+			showTodayLine = !showTodayLine;
+			return gantt;
+		};
+
+		gantt.showTodayFormat = function(todayFormat) {
+			if (!arguments.length) return showTodayFormat;
+			showTodayFormat = todayFormat;
+			return gantt;
+		};
+
+		gantt.colorProperty = function(colorProp) {
+			if (!arguments.length) return colorPropertyName;
+			colorPropertyName = colorProp;
+			return gantt;
+		};
+
+		gantt.rowSeparators = function (color) {
+			if (!arguments.length) return rowSeparatorsColor;
+			rowSeparatorsColor = color;
+			return gantt;
+
+		};
+
+		gantt.background = function (color) {
+			if (!arguments.length) return backgroundColor;
+			backgroundColor = color;
+			return gantt;
+		};
+
+		gantt.showTimeAxis = function () {
+			showTimeAxis = !showTimeAxis;
+			return gantt;
+		};
+
+		gantt.showTimeAxisTick = function () {
+			timeAxisTick = !timeAxisTick;
+			return gantt;
+		};
+
+
+
+		return gantt;
+
+	};
+
+})();
